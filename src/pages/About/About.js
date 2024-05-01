8c6cee4d03732c3e445e566fd8e662800926b4de
mvitale2 <122138703+mvitale2@users.noreply.github.com>
2024-04-30 20:08:01 -0400

    all pages have infrastructure built. next steps are to add mobile styling and party pages.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+import descriptions from "./descriptions.js";
+import "./About.css";
+import Afastera from "../../assets/Afastera.jpg";
+import Korinsted from "../../assets/Luren.png";
+import Polvi from "../../assets/polvi.png"
+
+function Continent({ name, description, image }) {
+  return (
+    <div className="continent">
+      <div className="left">
+        <img src={image} alt={name} />
+      </div>
+      <div className="right">
+        <h2>{name}</h2>
+        <hr />
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="about-wrapper">
+      <section className="world-desc">
+        <p>{descriptions[0]}</p>
+        <p>
+          Vahora is made up of 10 continents, each of which are described
+          further below:
+        </p>
+        <ul>
+          <li>Afastera</li>
+          <li>Awlerpar</li>
+          <li>Campooros</li>
+          <li>Creas'ol</li>
+          <li>Charkañas</li>
+          <li>Corinstead</li>
+          <li>Doblear</li>
+          <li>Korinsted</li>
+          <li>Polvinagas</li>
+          <li>The Southern Mass</li>
+        </ul>
+      </section>
+      <section className="continents-wrapper">
+        <h1>Browse Vahora's Continents</h1>
+        <Continent
+          name="Afastera"
+          image={Afastera}
+          description={descriptions[1]}
+        />
+        <Continent
+          name="Korinsted"
+          image={Korinsted}
+          description={descriptions[2]}
+        />
+        <Continent
+          name="Polvinagas"
+          image={Polvi}
+          description={descriptions[3]}
+        />
+      </section>
+    </div>
+  );
 }
 
-export default About
+export default About;
---
+++ src/pages/About/descriptions.js
@@ -0,0 +1,8 @@
+const descriptions = [
+  "Vahora is an incredibly ancient world with a history spanning eons. The main premise is that each planet in the star system that Vahora inhabits is a deity. The people of Vahora undergo cycles separated into different Ages. Each Age is characterized by a rise in technological progress over centuries. Eventually, this technology proves to be the peoples' undoing, leading to a catastrophic event and effectively resetting the whole world. Most of the characters contained within this archive lived in the Age of Exemplars.",
+  "Afastera is a wasteland of dust.",
+  "Korinsted is a more or less boreal continent with a large undead population. The most prominent city on the continent is Allahain, a city run entirely by houses of undead. The enture region of Luren has been beset with a curse that causes every living thing to rise as an undead upon death.",
+  "Polvinagas is a desert with storm crystals that fuel trade and magic items alike.",
+];
+
+export default descriptions;
